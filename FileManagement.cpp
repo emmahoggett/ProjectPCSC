5f936eabff91bbca32202ffe0c7d0a0c611831d0
emmahoggett <emma.hoggett@epfl.ch>
2019-11-25 11:42:45 +0100

    File management creation

---
+++ FileManagement.cpp
@@ -0,0 +1,9 @@
+/*
+ * FileManagement.cpp
+ *
+ *  Created on: Nov 25, 2019
+ * 	   Authors: Hoggett Emma <emma.hoggett@epfl.ch> &
+ * 	            Vuillecard Pierre <pierre.vuillecard@epfl.ch>
+ */
+
+
---
+++ FileManagement.hpp
@@ -0,0 +1,12 @@
+/*
+ * FileManagement.hpp
+ *
+ *  Created on: Nov 25, 2019
+ * 	   Authors: Hoggett Emma <emma.hoggett@epfl.ch> &
+ * 	            Vuillecard Pierre <pierre.vuillecard@epfl.ch>
+ */
+
+#ifndef FILEMANAGEMENT_HPP_
+#define FILEMANAGEMENT_HPP_
+
+#endif /*FILEMANAGEMENT_HPP_*/
