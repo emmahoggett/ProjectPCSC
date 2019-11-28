def413af16c4b1c5d49c2aebb34de1021e94902e
emmahoggett <emma.hoggett@epfl.ch>
2019-11-28 18:09:30 +0100

    debug get samples

--- Normal.cpp
+++ Normal.cpp
@@ -1,8 +1,14 @@
-//
-// Created by pierr on 25/11/2019.
-//
-#include "Normal.h
+/*
+ * Normal.cpp
+ *
+ *  Created on: Nov 22, 2019
+ * 	   Authors: Hoggett Emma <emma.hoggett@epfl.ch> &
+ * 	            Vuillecard Pierre <pierre.vuillecard@epfl.ch>
+ */
+
+#include "Normal.h"
 #include <iostream>
+#include <cmath>
 #include <boost/math/special_functions/erf.hpp>
 Normal :: Normal(const unsigned int N ) :
 Uniform(N) ,mean_normal(0.0) , var_normal(1.0)
@@ -13,24 +19,10 @@ Uniform(N) ,mean_normal(0.0) , var_normal(1.0)
 }
 
 
-Normal :: Normal(const unsigned int N , const double mu , const double var)
+Normal :: Normal(const unsigned int N , const double mu , const double var):
 Uniform(N) ,mean_normal(mu) , var_normal(var)
 {
     for (int i = 0; i < N; ++i) {
         N_m.push_back( mu + sqrt(var)*sqrt(2)*erfc_inv(2*U_m[i]) );
     }
-}
-
-// Get function
-Normal :: std::vector<double> get_sample() const
-{
-    return N_m ;
-}
-Normal :: virtual double get_mean() const
-{
-    return mean_normal;
-}
-Normal :: virtual double get_var() const
-{
-    return var_normal ;
 }
--- Normal.h
+++ Normal.h
@@ -1,6 +1,10 @@
-//
-// Created by pierr on 23/11/2019.
-//
+/*
+ * Normal.h
+ *
+ *  Created on: Nov 22, 2019
+ * 	   Authors: Hoggett Emma <emma.hoggett@epfl.ch> &
+ * 	            Vuillecard Pierre <pierre.vuillecard@epfl.ch>
+ */
 
 #ifndef MONTE_CARLO_NORMAL_GENERATOR_H
 #define MONTE_CARLO_NORMAL_GENERATOR_H
@@ -14,9 +18,9 @@ public:
     Normal(const unsigned int N , const double mu , const double var);
 
     // Get function
-    std::vector<double> get_sample() const ;
-    virtual double get_mean() const ;
-    virtual double get_var() const ;
+    std::vector<double> get_sample() { return N_m;}
+    virtual double get_mean() { return mean_normal;}
+    virtual double get_var() { return var_normal;}
 
 private:
     std::vector<double> N_m ;
