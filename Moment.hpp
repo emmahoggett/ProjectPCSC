a6d74fd3263c3e8d98ea96529770b8f63d4983c3
emmahoggett <emma.hoggett@epfl.ch>
2019-11-29 10:26:55 +0100

    add comments

--- Moment.hpp
+++ Moment.hpp
@@ -19,10 +19,17 @@
 
 using namespace std;
 class Moment{
+    /*
+     * Compute the moment of sample until the order given by the user.
+     * The result is then put into an output stream
+     *
+     */
 public:
+    // Constructor and destructor
     Moment();
     virtual ~Moment();
 
+    // Public method that compute moment of the sample until the order. The result is
     void getMoment(ofstream& stream, Random_variable* sample, const int order)const;
 
 };
