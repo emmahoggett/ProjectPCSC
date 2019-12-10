9a7135ac31a0029122a3612b5c6c1c7f74d84816
emmahoggett <emma.hoggett@epfl.ch>
2019-12-10 13:29:13 +0100

    control error

--- graph.py
+++ graph.py
@@ -29,9 +29,17 @@ sns.set(style = "whitegrid")
 ax = sns.lineplot(N, convCTL['LowerBound'])
 ax = sns.lineplot(N, convCTL['UpperBound'])
 ax = sns.lineplot(N, convCTL['mcmean'])
+if 'truemean' in convCTL.columns:
+    ax = sns.lineplot(N, convCTL['truemean'])
 
 # Add legend, title and labels
-ax.legend(['LowerBound','UpperBound', 'Mean of the vector'])
+    
+if 'truemean' in convCTL.columns:
+    ax.legend(['LowerBound','UpperBound', 'Mean of the vector', 'True Mean'])
+    
+else:
+    ax.legend(['LowerBound','UpperBound', 'Mean of the vector'])
+    
 ax.set(ylabel = 'Central limit mean values', xlabel= 'Size of the uniform & normal vector',
 title = 'Convergence of the central limite theorem values as a function of N')
 
